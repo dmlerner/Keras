780e476e810f2aba7e5c53274d865457b21dfca0
David Lerner <dmlerner@gmail.com>
2017-08-19 00:56:12 -0700

    .

--- data_ilsvrc.py
+++ data_ilsvrc.py
@@ -8,14 +8,21 @@ from keras.layers.core import *
 from keras import backend as K
 
 root = '/media/david/1600E62300E60997/ILSVRC/cropped/'
-generator = ImageDataGenerator().flow_from_directory(root, class_mode='categorical', batch_size=10)
+generator = ImageDataGenerator(
+    samplewise_center=True,
+    samplewise_std_normalization=True,
+    zca_whitening=True,
+    horizontal_flip=True,
+).flow_from_directory(root, class_mode='categorical', batch_size=10)
 
 inputs = Input(shape=(256,256,3))
 convolution = Conv2D(20, (5,5))(inputs)
 maxpool = MaxPooling2D(2)(convolution)
 flat = Flatten()(maxpool)
-x = Dense(100, activation='relu', use_bias=True)(flat)
-outputs = Dense(569, activation='sigmoid', use_bias=True)(x)
+d1 = Dense(500, activation='relu', use_bias=True)(flat)
+d2 = Dense(500, activation='sigmoid', use_bias=True)(d1)
+outputs = Dense(569, use_bias=True)(d1)
 model = Model(inputs=inputs, outputs=outputs)
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+model.compile(optimizer='rmsprop', loss='mean_squared_error')
 model.fit_generator(generator, epochs=100, steps_per_epoch=10)
+#todo: implement % correct measurement
