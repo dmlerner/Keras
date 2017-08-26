3bfb722d016881a6ba5025f2cb37ed6ab2ce7abd
David Lerner <dmlerner@gmail.com>
2017-08-26 11:49:24 -0700

    graphs with lines but not learning much

--- data_ilsvrc.py
+++ data_ilsvrc.py
@@ -1,4 +1,6 @@
+import numpy
 import keras
+from math import log10
 from keras.preprocessing.image import ImageDataGenerator
 from numpy import array, argmax, apply_along_axis
 from keras.regularizers import *
@@ -11,7 +13,7 @@ from keras.layers.convolutional import *
 from keras.layers.core import *
 from keras import *
 from keras import backend as K
-from pylab import plot, ion, draw, pause, subplot
+from pylab import plot, ion, draw, pause, subplot, ylim
 from keras.layers.normalization import BatchNormalization
 import itertools
 
@@ -42,8 +44,8 @@ def sample(flow, n):
     #does flow basically have this built in?
 
 
-flow_train = generator.flow_from_directory(root+'train', class_mode='categorical', batch_size=100)
-flow_test = generator.flow_from_directory(root+'test', class_mode='categorical', batch_size=100)
+flow_train = generator.flow_from_directory(root+'train', class_mode='categorical', batch_size=10)
+flow_test = generator.flow_from_directory(root+'test', class_mode='categorical', batch_size=10)
 
 #fit_images = sample(flow_train, 1000)[0]
 #generator.fit(fit_images)
@@ -60,15 +62,19 @@ def dense_suite(inputs, n, activation='relu'):
 	dense = Dense(n, activation=activation, use_bias=True)(inputs)
 	return BatchNormalization()(dense)
 
-inputs = Input(shape=(256,256,3))
-flat = Flatten()(inputs)
-dense = dense_suite(flat, 569)
-#dense = Dense(569)(flat)
-outputs = Activation('sigmoid')(dense)
-model = Model(inputs=inputs, outputs=outputs)
 
-sgd = optimizers.adagrad()
-model.compile(optimizer=sgd, loss='categorical_crossentropy')
+inputs = Input(shape=(256,256,3))
+conv1 = conv_suite(inputs, 20, 3)
+conv2 = conv_suite(conv1, 40, 5)
+conv3 = conv_suite(conv2, 80, 5)
+flat = Flatten()(conv2)
+dense1 = dense_suite(flat, 1024)
+activation = Activation('relu')(dense1)
+dense2 = dense_suite(activation, 569)
+outputs = Activation('softmax')(dense2)
+
+model = Model(inputs=inputs, outputs=outputs)
+model.compile(optimizer=optimizers.nadam(), loss='categorical_crossentropy')
 function = K.function([model.input]+[K.learning_phase()], [model.layers[-1].output])
 
 def percent_correct(flow, n):
@@ -82,8 +88,8 @@ def percent_correct(flow, n):
         samples.append(percent_correct)
     return sum(samples) / len(samples)
 
-p_train = [0]
-p_test = [0]
+p_train = []
+p_test = []
 train_loss, test_loss = [], []
 skip = 5
 ion()
@@ -93,15 +99,18 @@ for epoch in range(10000):
 		p_train.append(percent_correct(flow_train, 1000))
 		p_test.append(percent_correct(flow_test, 1000))
 		subplot(121, axisbg='black')
+		plot(t[::skip], p_test, 'w.')
 		plot(t[::skip], p_train, 'r.')
-		plot(t[::skip], p_test, 'b.')
 		subplot(122, axisbg='black')
-		plot(t, train_loss, 'r.')
-		plot(t, test_loss, 'b.')
+		n_fit = 100
+		linear_fit = numpy.poly1d(numpy.polyfit(t[-n_fit:], train_loss[-n_fit:], 1))
+		plot(t, array(test_loss), 'w.', markersize=10)
+		plot(t, array(train_loss), 'r.', markersize=10)
+		plot(t[-n_fit:], linear_fit(t[-n_fit:]), 'y')
 		print('.'*15, epoch, p_train[-1], p_test[-1])
 		pause(.01)
 		model.save('keras.sav')
-	history = model.fit_generator(flow_train, validation_data=flow_test, steps_per_epoch=10, validation_steps=1)
-	train_loss.append(history.history['loss'])
-	test_loss.append(history.history['val_loss'])
+	history = model.fit_generator(flow_train, validation_data=flow_test, steps_per_epoch=100, validation_steps=1)
+	train_loss.append(history.history['loss'][0])
+	test_loss.append(history.history['val_loss'][0])
 
